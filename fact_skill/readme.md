a797c7eb201f627b2722cfd9b4c517159abc879f
Jatin <jatinrohilla69@gmail.com>
2018-01-31 23:32:08 +0530

    a few reference skills

---
+++ fact_skill/readme.md
@@ -0,0 +1,3 @@
+# Fact skill
+
+[Amazon's Fact Skill tutorial](https://developer.amazon.com/alexa-skills-kit/tutorials/fact-skill-1)
---
+++ fact_skill/sample_response.js
@@ -0,0 +1,24 @@
+{
+  "version": "1.0",
+  "response": {
+    "outputSpeech": {
+      "ssml": "<speak> Here's your fact: A year on Mercury is just 88 days long. </speak>",
+      "type": "SSML"
+    },
+    "card": {
+      "content": "A year on Mercury is just 88 days long.",
+      "title": "Space Facts"
+    },
+    "speechletResponse": {
+      "outputSpeech": {
+        "ssml": "<speak> Here's your fact: A year on Mercury is just 88 days long. </speak>"
+      },
+      "card": {
+        "content": "A year on Mercury is just 88 days long.",
+        "title": "Space Facts"
+      },
+      "shouldEndSession": true
+    }
+  },
+  "sessionAttributes": {}
+}
